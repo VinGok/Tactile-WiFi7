d3d86b3c4589824c7704fb76f5282e65139b7beb
VinGok <32326011+VinGok@users.noreply.github.com>
2021-08-07 19:27:59 +0200

    Update main.cpp

--- Vanilla-WiFi7/main.cpp
+++ Vanilla-WiFi7/main.cpp
@@ -1,17 +1,7 @@
-// Haptic and video streams being transmitted from each STA as AC_HA and AC_VI, respectively.
-// Both streams generate frames at 1kHz
 // OFDMA on DL and UL in batches of 4 or 2 or 1 with highest-first Tx.
 // Project used for results in High-five 2.0 paper
 
-// Full OFDMA and short retry count implemented for buffered scheme (BuS)
 // RMSE measurement for DL by isolating latency experienced by one particular stream
-
-
-// Commands for synchronizing with Github
-// git add 80211ax-TI-OFDMA-Win
-// git commit -m "comment"
-// git push
-// git status
 
 
 #include <stdio.h>
